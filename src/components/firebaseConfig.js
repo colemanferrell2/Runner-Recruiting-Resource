63dc5f3d24b92240fa3471c23f9784dc6a1f29c2
colemanferrell2 <caferr@unc.edu>
2024-12-20 11:02:32 -0500

    Firebase Auth config

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,17 +1,22 @@
+// Import necessary Firebase modules
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyAWXflFFAj-gMweIXOuhHQy5bLWS7y7rOA",
+  authDomain: "runrecruit-1f28a.firebaseapp.com",
+  projectId: "runrecruit-1f28a",
+  storageBucket: "runrecruit-1f28a.firebasestorage.app",
+  messagingSenderId: "799884349482",
+  appId: "1:799884349482:web:542666c35c8763847bf353",
+  measurementId: "G-DNJ4JKJVSB",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
